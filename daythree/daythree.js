dc3ee5fc7d15946755686b656f37b083ac4be57f
shraddhab399 <92900507+shraddhab399@users.noreply.github.com>
2023-04-19 13:13:57 +0530

    Add files via upload

---
+++ daythree/daythree.js
@@ -0,0 +1,55 @@
+// it will give alert message
+// alert("congratulations");
+// alert("hello");
+// alert(4+2);
+// let num=15;
+// alert(num)
+// const num2=13;
+// alert(num2);
+// num2=122;
+// alert(num2);
+// var num3=45;
+// alert(num3);
+
+//assignment operator
+let a=13;
+a*=8;
+alert(a);
+
+let c=6;
+c+=4;
+alert(c);
+console.log(c)
+//relationa opertor
+let res=12>0;
+alert(res);
+
+//logical operator
+let result=3<9 && 6<9;
+alert(result)
+
+var x=12;
+var y=90;
+alert(x==y && x>y)
+//if else
+
+if(x<y)
+alert("shraddha");
+else
+alert("bhor")
+
+alert(typeof(1))
+
+alert(typeof("shrddha"))
+
+let age=prompt("whats your age?",100)//two parameter 1st statement 2nd answer of the question
+alert("your age is "+age)
+
+function myFunction() {
+    var x = document.getElementById("download.jpg").src;
+    document.getElementById("demo").innerHTML = x;
+}
+// // 
+// var image = document.getElementById("myImage").src="download.jpg";
+// alert(""+image );
+// alert("image added succesfully");
---
+++ daythree/intro.js
@@ -0,0 +1,6 @@
+function printlength(){
+    var str=document.getElementById("name").value;
+    var strlen=str.length;
+    document.getElementById("demo").innerHTML="Your score is:"+strlen;
+
+}
